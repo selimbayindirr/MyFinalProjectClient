ab87bf6abc356be74f776e9227b5629946942397
byndr <byndr28@gmail.com>
2023-04-10 01:39:03 +0300

    Grid

--- northwind/src/app/app.module.ts
+++ northwind/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NaviComponent } from './navi/navi.component';
+import { CategoryComponent } from './category/category.component';
+import { ProductComponent } from './product/product.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NaviComponent,
+    CategoryComponent,
+    ProductComponent
   ],
   imports: [
     BrowserModule,
